2ac63e4dd4adc49246a45a20be01961baafb99cc
mayconr4 <mayconhs2007@gmail.com>
2025-06-17 08:53:40 -0300

    saidas e compilações

---
+++ 01-hello-world.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var nome = "fulano de tal";
+// const idade: number = 18;
+// const condicao:boolean = true;
+console.log("Ol\u00E1 ".concat(nome, "! Bem-vindo(a) ao TS"));
+// para compilar tsc + nome do arquivo/pasta
--- 01-hello-world.ts
+++ 01-hello-world.ts
@@ -1,5 +1,8 @@
-const nome: string = "Maycon";
+export {};
+
+const nome: string = "fulano de tal";
 // const idade: number = 18;
 // const condicao:boolean = true;
 
 console.log(`Olá ${nome}! Bem-vindo(a) ao TS`);
+// para compilar tsc + nome do arquivo/pasta
